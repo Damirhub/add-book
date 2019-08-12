cbffd23eb0a47b251c7cf54a04fa2d278beb0944
Damirhub <damir.pjetlovic@gmail.com>
2019-08-12 17:33:32 +0200

    setup

---
+++ src/containers/Mockup.js
@@ -0,0 +1,102 @@
+import React, { useState } from "react";
+import * as ACTIONS from "../store/actions/actions";
+import { failure } from "../store/actions/actions";
+
+import { connect } from "react-redux";
+import init from '../config'
+
+const Mockup = ({
+    stateprop1,
+    stateprop2,
+    action1,
+    action2,
+    action_creator1,
+    action_creator2,
+    user_input,
+    input_text,
+    ...restOfProps
+}) => {
+
+
+    const [genre, setGenre] = useState('')
+
+    const handleClick = (G) => {
+        console.log("GGGG", G)
+    }
+
+    console.log("ZZZ", genre)
+    // console.log( init.genres.map((x, y) => { x[y] })   )
+    return (
+        <>
+            {/* {init.genres.map(x => {
+                return (
+                    <button onClick={handleClick(x.name)} key={x.id} >
+                        {x.name} </button>)
+            })} */}
+
+            {init.genres.map(x => {
+                return (
+                    <button onClick={() => handleClick(x)} key={x.id} >
+                        {x.name}
+                    </button>)
+            })}
+
+            {init.genres.map(x => {
+                return (
+                    <button onClick={() => setGenre(x)} key={x.id} >
+                        {x.name}
+                    </button>)
+            })}
+
+
+
+
+            <h4>Get State: {stateprop1.toString()}</h4>
+            <br />
+            <button onClick={() => action1()}>Dispatch Action 1</button>
+            <button onClick={() => action2()}>Dispatch Action 2</button>
+            <hr />
+            <button onClick={() => action_creator1()}>Action Creator 1 </button>
+            <button onClick={action_creator2}>Action Creator 2 </button>
+            <hr />
+            <form>
+                <input
+                    onChange={e => {
+                        user_input(e.target.value);
+                    }}
+                    placeholder="user input"
+                />
+            </form>
+            <br />
+            <h3>{input_text}</h3>
+            <hr />
+            {stateprop2}
+        </>
+    );
+};
+
+const mapStateToProps = state => {
+    return {
+        stateprop1: state.example.stateprop1,
+        stateprop2: state.example2.stateprop2,
+
+        input_text: state.example2.user_input
+    };
+};
+
+const mapDispatchToProps = dispatch => {
+    return {
+        action1: () => dispatch(ACTIONS.SUCCESS),
+        action2: () => dispatch(ACTIONS.FAILURE),
+
+        action_creator1: () => dispatch(ACTIONS.success()),
+        action_creator2: () => dispatch(failure()),
+
+        user_input: whatever => dispatch(ACTIONS.user_input(whatever))
+    };
+};
+
+export default connect(
+    mapStateToProps,
+    mapDispatchToProps
+)(Mockup);
---
+++ src/store/reducers/reducer1.js
@@ -0,0 +1,35 @@
+import * as ACTION_TYPES from "../actions/action_types";
+
+import initial from '../../config.json'
+
+console.log("XXX", initial)
+
+const initialState = {
+    stateprop1: false,
+    user_input: ""
+};
+
+const reducer1 = (state = initialState, action) => {
+    switch (action.type) {
+        case ACTION_TYPES.SUCCESS:
+            return {
+                ...state,
+                stateprop1: true
+            };
+        case ACTION_TYPES.FAILURE:
+            return {
+                ...state,
+                stateprop1: false
+            };
+        case ACTION_TYPES.USER_INPUT:
+          console.log("ACTION: ", action);
+          return {
+            ...state,
+            user_input: action.text
+          };
+        default:
+            return state;
+    }
+};
+
+export default reducer1;
