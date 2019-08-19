e5a8d2cf848b1ec3e9391f8c08f270cb8f3a10cd
Damirhub <damir.pjetlovic@gmail.com>
2019-08-19 18:02:22 +0200

    some styles

--- src/containers/UI/Wizard.js
+++ src/containers/UI/Wizard.js
@@ -23,32 +23,37 @@ const Wizard = ({ pageCount, pageCounter, wizardStep }) => {
                 <h1 style={{ "background": "red" }} >{wizardStep} </h1>
             }
 
-<div className = "wizard-container">
-            <div className = "wizard-circle">1
-                <p>Genre</p>
-            </div>
-            <div className = "wizard-dash"/>
-            <div className = "wizard-circle">2
-                 <p>Subgenre</p>
-            </div>
-            <div className = "wizard-dash"/>
-            <div className = "wizard-circle">3
-                <p>Add new subgenre</p>
+            <div className="wizard-container">
+                <div className= {"wizard-circle" + (pageCount == 1 && " wizard-circle-active")} >
+                    <span>1</span>
+                    <p>Genre</p>
+                </div>
+                <div className="wizard-dash" />
+                <div className="wizard-circle">
+                    <span>2</span>
+                    <p>Subgenre</p>
+                </div>
+                <div className="wizard-dash" />
+                <div className="wizard-circle">
+                    <span>3</span>
+                    <p>Add new subgenre</p>
 
-            
-            </div>
-            <div className = "wizard-dash"/>
-            <div className = "wizard-circle">4
-             <p>Information</p>
-            
-            </div>
-</div>
 
-{/* <button onClick = { () => pageCounter(1)} >  NUSSUSSU </button>
+                </div>
+                <div className="wizard-dash" />
+                <div className="wizard-circle">
+                    <span>4</span>
+
+                    <p>Information</p>
+
+                </div>
+            </div>
+
+            {/* <button onClick = { () => pageCounter(1)} >  NUSSUSSU </button>
 <button onClick = { () => pageCounter(2)} >  NUSSUSSU </button>
 <button onClick = { () => pageCounter(3)} >  NUSSUSSU </button>
 <button onClick = { () => pageCounter(4)} >  NUSSUSSU </button> */}
-            
+
 
             <Steps />
         </Wrapper>
